d9ecabe105dfe0223ec93611165a970ecca0efad
nelson cheng <nlscng.g@gmail.com>
2020-02-27 22:21:40 -0600

    Problem 9, fixing my mistaken, max sum of two from list, solution; the correct solution for the question is TBD...

--- python/problem-9/LargestNonAdjacentSum.py
+++ python/problem-9/LargestNonAdjacentSum.py
@@ -7,20 +7,32 @@
 # Follow-up: Can you do this in O(N) time and constant space?
 
 
-def find_sum(in_list: list):
+def find_max_sum_out_of_two(in_list: list):
+    # first attempt, I didn't read the question right,
+    # ended up finding a max sum of two non adjacent numbers from list
     if len(in_list) < 2:
         return 0
 
     cur_max = float("-inf")
     for i in range(2, len(in_list)):
-        cur_max = max(cur_max, in_list[i])
+        cur_max = max(cur_max, in_list[i - 2])
         in_list[i - 2] = cur_max + in_list[i]
 
     max_sum = max(in_list[:-2])
     return max_sum
 
 
-assert find_sum([]) == 0
-assert find_sum([1]) == 0
-assert find_sum([2, 4, 6, 2, 5]) == 13
-assert find_sum([5, 1, 1, 5]) == 10
+assert find_max_sum_out_of_two([]) == 0
+assert find_max_sum_out_of_two([1]) == 0
+assert find_max_sum_out_of_two([2, 4, 6, 2, 5]) == 11
+assert find_max_sum_out_of_two([5, 1, 1, 5]) == 10
+
+
+def find_sum(in_list):
+    return None
+
+
+# assert find_sum([]) == 0
+# assert find_sum([1]) == 0
+# assert find_sum([2, 4, 6, 2, 5]) == 13
+# assert find_sum([5, 1, 1, 5]) == 10

0846794b73cd72071b1e22c9810f988d370dbe22
nelson cheng <nlscng.g@gmail.com>
2020-03-23 09:07:08 -0500

    Problem 36, refactor to make generic, find nth smallest from BST.

--- problem-36/SecondLargestInBST.py
+++ problem-36/SecondLargestInBST.py
@@ -13,30 +13,38 @@ class NodeBST:
 
 
 def find_second_largest(node: NodeBST):
-    # helper func to flatten the tree
-    def in_order(node: NodeBST, accu: []):
-        if node is None:
-            return
-
-        if node.left is not None:
-            in_order(node.left, accu)
-
-        accu.append(node.value)
-
-        if node.right is not None:
-            in_order(node.right, accu)
-
     flatten = []
     in_order(node, flatten)
 
     return flatten[-2] if len(flatten) > 2 else None
 
 
+def in_order(node: NodeBST, accu: []):
+    if node is None:
+        return
+
+    if node.left is not None:
+        in_order(node.left, accu)
+
+    accu.append(node.value)
+
+    if node.right is not None:
+        in_order(node.right, accu)
+
+
+def find_nth_largest(node: NodeBST, nth: int):
+    flatten = []
+    in_order(node, flatten)
+
+    return flatten[-nth] if len(flatten) > nth else None
+
+
 test_tree_a = NodeBST(3,
                       NodeBST(1,
-                            right=NodeBST(2)),
+                              right=NodeBST(2)),
                       NodeBST(4,
-                            right=NodeBST(5)))
-
+                              right=NodeBST(5)))
 
 assert find_second_largest(test_tree_a) == 4
+assert find_nth_largest(test_tree_a, 2) == 4
+assert find_nth_largest(test_tree_a, 6) is None

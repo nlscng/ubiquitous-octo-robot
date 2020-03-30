cf7b0978e10764f38d1afc30c67e7e79c5e9c5c9
nelson cheng <nlscng.g@gmail.com>
2020-03-30 11:29:39 -0500

    Problem 18, max in sliding window, O(n) version from other's solution.

--- problem-18/MaximumInSubarray.py
+++ problem-18/MaximumInSubarray.py
@@ -13,6 +13,7 @@
 
 FLIP_SIGN = -1
 
+
 def find_max_in_subarray(liz: list, k: int):
     # this is tougher than I realized to achieve O(n)
     # my best without looking up is with a priority queue or some other max-heap type thing, but that is O(n log n)
@@ -20,7 +21,7 @@ def find_max_in_subarray(liz: list, k: int):
 
     import heapq
 
-    #TODO: incomplete and wrong, this pops the max but we don't need it to pop the max unless max is outside of the window
+    # TODO: incomplete and wrong, this pops the max but we don't need it to pop the max unless max is outside of the window
     heap = []
     heapq.heapify(heap)
     windowed_max = []
@@ -36,7 +37,37 @@ def find_max_in_subarray(liz: list, k: int):
     return windowed_max
 
 
-assert find_max_in_subarray([5, 2, 1], 2) == [5, 2]
-assert find_max_in_subarray([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8], "actual value is: {}".format(find_max_in_subarray([10, 5, 2, 7, 8, 7], 3))
+# assert find_max_in_subarray([5, 2, 1], 2) == [5, 2]
+# assert find_max_in_subarray([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8], "actual value is: {}".format(find_max_in_subarray([10, 5, 2, 7, 8, 7], 3))
 
 
+def find_max_in_subarray_ver2(liz: list, k: int):
+    # see this https://stackoverflow.com/questions/8031939/finding-maximum-for-every-window-of-size-k-in-an-array
+
+    assert len(liz) >= k, "length of list needs to be at least k."
+
+    left_mapped = []
+    right_mapped = []
+    cur_left_max, cur_right_max = 0, 0
+    for i in range(len(liz)):
+        if i % k == 0:
+            cur_left_max, cur_right_max = 0, 0
+        cur_left_max = max(cur_left_max, liz[i])
+        left_mapped.append(cur_left_max)
+
+        cur_right_max = max(cur_right_max, liz[-i - 1])
+        right_mapped.append(cur_right_max)
+
+    right_mapped.reverse()
+
+    max_liz = []
+    for i in range(len(liz) - k + 1):
+        max_liz.append(max(right_mapped[i], left_mapped[i + k - 1]))
+
+    return max_liz
+
+
+assert find_max_in_subarray_ver2([5, 2, 1], 2) == [5, 2], "actual value is: {}".format(
+    find_max_in_subarray_ver2([5, 2, 1, 2], 2))
+assert find_max_in_subarray_ver2([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8], "actual value is: {}".format(
+    find_max_in_subarray_ver2([10, 5, 2, 7, 8, 7], 3))

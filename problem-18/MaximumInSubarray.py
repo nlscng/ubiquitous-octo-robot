76ce2daef9f7b21f08af16ebced5925fc6006cf7
nelson cheng <nlscng.g@gmail.com>
2020-03-29 21:17:12 -0500

    Problem 18, max in subarray, incomplete.

---
+++ problem-18/MaximumInSubarray.py
@@ -0,0 +1,42 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was asked by Google.
+#
+# Given an array of integers and a number k, where 1 <= k <= length of the array, compute the maximum values of each
+# subarray of length k.
+#
+# For example, given array = [10, 5, 2, 7, 8, 7] and k = 3, we should get: [10, 7, 8, 8], since:
+#
+# 10 = max(10, 5, 2) 7 = max(5, 2, 7) 8 = max(2, 7, 8) 8 = max(7, 8, 7) Do this in O(n) time and O(k) space. You can
+# modify the input array in-place and you do not need to store the results. You can simply print them out as you
+# compute them.
+
+FLIP_SIGN = -1
+
+def find_max_in_subarray(liz: list, k: int):
+    # this is tougher than I realized to achieve O(n)
+    # my best without looking up is with a priority queue or some other max-heap type thing, but that is O(n log n)
+    assert len(liz) >= k, "length of list needs to be at least k."
+
+    import heapq
+
+    #TODO: incomplete and wrong, this pops the max but we don't need it to pop the max unless max is outside of the window
+    heap = []
+    heapq.heapify(heap)
+    windowed_max = []
+    for i in range(len(liz)):
+        if i < k - 1:
+            heap.append(liz[i] * FLIP_SIGN)
+            continue
+
+        heapq.heappush(heap, liz[i] * FLIP_SIGN)
+        from_heap = heapq.heappop(heap) * FLIP_SIGN
+        windowed_max.append(from_heap)
+
+    return windowed_max
+
+
+assert find_max_in_subarray([5, 2, 1], 2) == [5, 2]
+assert find_max_in_subarray([10, 5, 2, 7, 8, 7], 3) == [10, 7, 8, 8], "actual value is: {}".format(find_max_in_subarray([10, 5, 2, 7, 8, 7], 3))
+
+

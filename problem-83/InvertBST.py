22daab0c66c4647bac3a277e4272a966e10c34dd
nelson cheng <nlscng.g@gmail.com>
2020-05-25 12:31:58 -0500

    Problem 83, invert a bst.

---
+++ problem-83/InvertBST.py
@@ -0,0 +1,86 @@
+# Good morning! Here's your coding interview problem for today.
+#
+# This problem was asked by Google.
+#
+# Invert a binary tree.
+#
+# For example, given the following tree:
+#
+#     a
+#    / \
+#   b   c
+#  / \  /
+# d   e f
+# should become:
+#
+#   a
+#  / \
+#  c  b
+#  \  / \
+
+from common.treenode.MyBST import MyBstNodeStringVal
+
+
+def invert_bst(root: MyBstNodeStringVal) -> MyBstNodeStringVal:
+    if root is None:
+        return root
+
+    root.left, root.right = root.right, root.left
+
+    invert_bst(root.left)
+    invert_bst(root.right)
+    return root
+
+
+a = MyBstNodeStringVal('a')
+b = MyBstNodeStringVal('b')
+c = MyBstNodeStringVal('c')
+d = MyBstNodeStringVal('d')
+e = MyBstNodeStringVal('e')
+f = MyBstNodeStringVal('f')
+
+a_copy = a.deepcopy()
+assert invert_bst(a) == a_copy
+
+a.reset()
+b.reset()
+a_copy.reset()
+a_copy = a.deepcopy()
+b_copy = b.deepcopy()
+a.left = b
+a_copy.right = b_copy
+assert invert_bst(a) == a_copy
+
+a.reset()
+b.reset()
+a_copy = a.deepcopy()
+b_copy = b.deepcopy()
+c_copy = c.deepcopy()
+a.left = b
+a.right = c
+a_copy.right = b_copy
+a_copy.left = c_copy
+assert invert_bst(a) == a_copy
+
+a.reset()
+b.reset()
+a_copy = a.deepcopy()
+b_copy = b.deepcopy()
+c_copy = c.deepcopy()
+d_copy = d.deepcopy()
+e_copy = e.deepcopy()
+f_copy = f.deepcopy()
+
+a.left = b
+b.left = d
+b.right = e
+a.right = c
+c.left = f
+
+a_copy.left = c_copy
+c_copy.right = f_copy
+a_copy.right = b_copy
+b_copy.left = e_copy
+b_copy.right = d_copy
+
+assert invert_bst(a) == a_copy

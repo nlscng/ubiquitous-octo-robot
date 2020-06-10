1368f141a3b3e3fda218439b353040f75a43aa42
nelson cheng <nlscng.g@gmail.com>
2020-06-10 17:59:56 -0500

    Problem 72, most frequent occurence on path of a graph.

--- problem-72/MostFrequentOccurenceOnGraph.py
+++ problem-72/MostFrequentOccurenceOnGraph.py
@@ -28,27 +28,47 @@
 # [(0, 0)]
 # Should return null, since we have an infinite loop.
 
-def count_most_frequent(arr: list) -> int:
+def count_most_frequent(path: list, nodes: str) -> int:
     count = {}
-    for one_letter in arr:
-        if one_letter not in count:
-            count[one_letter] = 1
+    for one_stop in path:
+        key = nodes[one_stop]
+        if key not in count:
+            count[key] = 1
         else:
-            count[one_letter] += 1
-
-    highest_frequency = sorted(count.values())
+            count[key] += 1
+    highest_frequency = sorted(count.values(), reverse=True)
     return highest_frequency[0]
 
 
 def largest_value_path(nodes: str, edges: list) -> int:
-    walker = init_node = 0
-    path_history = {init_node}
-    for good_edge in [one_edge for one_edge in edges if one_edge[0] == walker]:
-        if good_edge[1] in path_history:
-            # detect cycle, and fail out early if so
+    cur_max = [0]
+    found_loop = [False]
+
+    def explore_recur(walker: int, path: list):
+        if found_loop[0]:
+            return True
+        cur_count = count_most_frequent(path, nodes)
+        cur_max[0] = max(cur_count, cur_max[0])
+
+        for src, tgt in [one_edge for one_edge in edges if one_edge[0] == walker]:
+            if tgt in path:
+                # detected cycle, and fail out early if so
+                found_loop[0] = True
+                break
+            path.append(tgt)
+            explore_recur(tgt, path)
+            path.pop()  # backtrack
+
+    for i in range(len(nodes)):
+        explore_recur(i, [i])
+        if found_loop[0]:
             return None
-        path_history.add(walker[1])
+
+    return cur_max[0]
 
 
-assert largest_value_path("A", [(0, 0)]) == 0
-assert largest_value_path("ABACA", [(0, 1), (0, 2), (2, 3), (3, 4)]) == 3
+# assert count_most_frequent([0,2,3,4], 'ABACA') == 3
+
+assert largest_value_path("A", [(0, 0)]) is None, "Actual :{}".format(largest_value_path("A", [(0, 0)]))
+res = largest_value_path("ABACA", [(0, 1), (0, 2), (2, 3), (3, 4)])
+assert res == 3, "Actual: {}".format(res)

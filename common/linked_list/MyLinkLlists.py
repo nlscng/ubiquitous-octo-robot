ef1c3513bef85a2236fe976336e697aea79ab3f2
nelson cheng <nlscng.g@gmail.com>
2020-07-01 16:29:39 -0500

    Problem 78, merge sorted linked lists into one sorted linked list.

--- common/linked_list/MyLinkLlists.py
+++ common/linked_list/MyLinkLlists.py
@@ -3,13 +3,23 @@ class SinglyLinkedListNode:
         self.value = value
         self.next = nxt
 
-    def set_next(self, nxt):
-        self.next = nxt
+    def set_next(self, next_node):
+        self.next = next_node
         return self
+
+    def chain_set_next(self, next_node):
+        self.next = next_node
+        return next_node
+
+    def get_next(self):
+        return self.next
 
     def set_value(self, value):
         self.value = value
         return self
+
+    def get_value(self):
+        return self.value
 
     def to_list(self, accu: list = None):
         if accu is None:
@@ -23,3 +33,30 @@ class SinglyLinkedListNode:
 
     def __str__(self):
         return str(self.value) + str(self.next) if self.next is not None else str(self.value)
+
+    def __eq__(self, other):
+        if other is None or not isinstance(other, SinglyLinkedListNode):
+            return False
+
+        if self.value != other.get_value():
+            return False
+
+        return self.next == other.get_next()
+
+
+n0 = SinglyLinkedListNode(0)
+n1 = SinglyLinkedListNode(1)
+n2 = SinglyLinkedListNode(2)
+
+a0 = SinglyLinkedListNode(0)
+a1 = SinglyLinkedListNode(1)
+a2 = SinglyLinkedListNode(2)
+assert n0 == n0
+assert n0 != n1
+
+n0.chain_set_next(n1).chain_set_next(n2)
+a0.chain_set_next(a1).chain_set_next(a2)
+assert n0 == a0
+
+a1.set_value(42)
+assert n0 != a0
---
+++ problem-78/MergeSinglyLinkedLists.py
@@ -0,0 +1,80 @@
+# This problem was asked by Google.
+#
+# Given k sorted singly linked lists, write a function to merge all the lists into one sorted singly linked list.
+
+'''
+3 6, 9
+2, 5, 8
+1, 7
+
+'''
+
+
+from common.linked_list.MyLinkLlists import SinglyLinkedListNode as SLLN
+import sys
+
+
+def merge(roots: list) -> SLLN:
+    # GG: tricky to get all corner cases right, especially handling multiple incoming LS, while creating one for result
+    # O(m) in time, m being len of all incoming linked list combined, and same for space complexity, O(m)
+    if not list:
+        return None
+    if len(roots) == 1:
+        return roots[0]
+
+    res_head = res_tail = None
+    while len(roots) > 0:
+        # find min out of current heads
+        cur_min_node = None
+        cur_min_idx = -1
+        cur_min_val = sys.maxsize
+        for idx, one_root in enumerate(roots):
+            if one_root.value < cur_min_val:
+                cur_min_node = one_root
+                cur_min_val = cur_min_node.get_value()
+                cur_min_idx = idx
+
+        # remove current min heads from list of lists
+        if cur_min_node.get_next() is None:
+            roots.remove(cur_min_node)
+        else:
+
+            roots[cur_min_idx] = cur_min_node.get_next()
+            cur_min_node.set_next(None)
+
+        if res_head is None:
+            res_tail = SLLN(cur_min_node.get_value())
+            res_head = res_tail
+        else:
+            res_tail.set_next(SLLN(cur_min_node.get_value()))
+            res_tail = res_tail.get_next()
+
+    return res_head
+
+
+assert merge([]) is None
+
+n0 = SLLN(0)
+n1 = SLLN(1)
+n2 = SLLN(2)
+n3 = SLLN(3)
+n4 = SLLN(4)
+n5 = SLLN(5)
+n6 = SLLN(6)
+n7 = SLLN(7)
+n8 = SLLN(8)
+n9 = SLLN(9)
+
+# assert merge([n1]) == n1
+
+list_238 = n2
+n2.chain_set_next(n3).chain_set_next(n8)
+
+list_147 = n1
+n1.chain_set_next(n4).chain_set_next(n7)
+
+list_0569 = n0
+n0.chain_set_next(n5).chain_set_next(n6).chain_set_next(n9)
+
+a = SLLN(0, SLLN(1, SLLN(2, SLLN(3, SLLN(4, SLLN(5, SLLN(6, SLLN(7, SLLN(8, SLLN(9))))))))))
+assert merge([list_147, list_238, list_0569]) == a

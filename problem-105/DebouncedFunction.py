3c0a01ab0a2f614a75d870a2ff3a4121dc31137e
nelson cheng <nlscng.g@gmail.com>
2020-07-20 15:32:04 -0500

    Problem 105, TBC.

---
+++ problem-105/DebouncedFunction.py
@@ -0,0 +1,9 @@
+# This problem was asked by Facebook.
+#
+# Given a function f, and N return a debounced f of N milliseconds.
+#
+# That is, as long as the debounced f continues to be invoked, f itself will not be called for N milliseconds.
+
+
+# GG: I started out clusless as to how to do this. Look into debounce vs throttling too, it might be fun
+# TBC

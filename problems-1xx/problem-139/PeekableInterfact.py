7aa79bd9037f049f66710c216546238b545ff59d
nelson cheng <nlscng.g@gmail.com>
2020-09-08 20:13:38 -0500

    Problem 139, peekable iterator interface from wrapping an iterator.

---
+++ problems-1xx/problem-139/PeekableInterfact.py
@@ -0,0 +1,36 @@
+# Given an iterator with methods next() and hasNext(), create a wrapper iterator, PeekableInterface, which also
+# implements peek(). peek shows the next element that would be returned on next().
+#
+# Here is the interface:
+# class PeekableInterface(object):
+#     def __init__(self, iterator):
+#         pass
+#
+#     def peek(self):
+#         pass
+#
+#     def next(self):
+#         pass
+#
+#     def hasNext(self):
+#         pass
+
+
+class PeekableInterface(object):
+    def __init__(self, iterator):
+        assert iterator is not None
+        self.iterator = iterator
+        self.my_next = iterator.next()
+
+    def peek(self):
+        return self.my_next
+
+    def next(self):
+        res = self.my_next
+        self.my_next = self.iterator.next()
+        return res
+
+    def hasNext(self):
+        return self.my_next is not None
+
+

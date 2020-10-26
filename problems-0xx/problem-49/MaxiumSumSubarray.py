74fd89899e3a29b0739b8d579260962edc01f64c
nelson cheng <nlscng.g@gmail.com>
2020-10-25 20:35:10 -0500

    Problem 49, maxium sum in a continous subarray of int, adding a cleaner dynamic programming version.

--- problems-0xx/problem-49/MaxiumSumSubarray.py
+++ problems-0xx/problem-49/MaxiumSumSubarray.py
@@ -34,5 +34,27 @@ def find_max_sum_subarray(arr: list) -> int:
 
 assert find_max_sum_subarray([1, -1, 1, -1]) == 1
 assert find_max_sum_subarray([1, 2, 0, 3, 4]) == 10
+assert find_max_sum_subarray([1, 2, -4, 3, 4]) == 7
 assert find_max_sum_subarray([34, -50, 42, 14, -5, 86]) == 137
 assert find_max_sum_subarray([-5, -1, -8, -9]) == 0
+
+
+def max_sum_subarray_dp(lis: list) -> int:
+    # This is O(n) in time and O(n) in space
+    assert lis
+    n = len(lis)
+
+    memo = [0] * n
+    memo[0] = max(lis[0], 0)
+
+    for i in range(1, n):
+        memo[i] = max(lis[i], lis[i] + memo[i - 1])
+
+    return max(memo)
+
+
+assert max_sum_subarray_dp([1, -1, 1, -1]) == 1
+assert max_sum_subarray_dp([1, 2, 0, 3, 4]) == 10
+assert max_sum_subarray_dp([1, 2, -4, 3, 4]) == 7
+assert max_sum_subarray_dp([34, -50, 42, 14, -5, 86]) == 137
+assert max_sum_subarray_dp([-5, -1, -8, -9]) == 0

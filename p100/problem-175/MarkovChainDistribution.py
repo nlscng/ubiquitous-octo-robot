3b29c280216a7058f20f00bf59fcf4c9d1967a46
nelson cheng <nlscng.g@gmail.com>
2020-11-07 11:30:54 -0600

    Problem 175, markov chain distribution. Used pandas in solution.

--- p100/problem-175/MarkovChainDistribution.py
+++ p100/problem-175/MarkovChainDistribution.py
@@ -18,3 +18,54 @@
 # One instance of running this Markov chain might produce { 'a': 3012, 'b': 1656, 'c': 332 }.
 
 # TBC
+
+# GG: this is a stationary markov chain distribution problem, GT lectures covered the basis for it.
+
+# GG: good question to review
+
+import pandas as pd
+
+
+def to_adj_list(matrix: list, keys: list) -> list:
+    assert matrix
+    n = len(keys)
+    key_idx = {k: i for i, k in enumerate(keys)}
+    res = [[0 for _ in range(n)] for _ in range(n)]
+    for one_entry in matrix:
+        src, snk, prob = one_entry
+        src_idx = key_idx[src]
+        snk_idx = key_idx[snk]
+        res[src_idx][snk_idx] = prob
+    return res
+
+
+m1 = [
+    ('a', 'a', 0.9),
+    ('a', 'b', 0.075),
+    ('a', 'c', 0.025),
+    ('b', 'a', 0.15),
+    ('b', 'b', 0.8),
+    ('b', 'c', 0.05),
+    ('c', 'a', 0.25),
+    ('c', 'b', 0.25),
+    ('c', 'c', 0.5)
+]
+print(to_adj_list(m1, ['a', 'b', 'c']))
+
+
+def markov_chain_n_step(matrix: list, n: int) -> list:
+    assert matrix and n and isinstance(matrix[0], tuple)
+    keys = sorted(set([k for k, _, _ in matrix]))
+    adj_list = to_adj_list(matrix, keys)
+
+    df = pd.DataFrame(adj_list)
+    res = df
+    for _ in range(n):
+        res = res @ df
+
+    print('res ' + str(res))
+
+    return (res.iloc[0, :] * n).tolist()
+
+
+print(markov_chain_n_step(m1, 5000))

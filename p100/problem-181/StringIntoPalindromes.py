8f2b1bbdbacefa12b8459a4591e3f8fd22d912f9
nelson cheng <nlscng.g@gmail.com>
2020-11-13 13:29:19 -0600

    Problem 181, string to fewest palindromes, using two 2D memo table to keep track of if substring is a palindrome, and how many palindrome is needed to cover the substring. TBC.

--- p100/problem-181/StringIntoPalindromes.py
+++ p100/problem-181/StringIntoPalindromes.py
@@ -8,8 +8,10 @@
 
 #GOOGLE
 #DP
+#DPMatrixMul
+#PalindromePartitioning
+
 # GG: This took a lot of effort for even the naive solution, and I don't have the back tracking done yet.
-# TBC
 """
 Not sure where to start at first. This smells like a dynamic programming problem though
 
@@ -35,3 +37,53 @@ c(i, j) = {
 This would get me the minimum num of palindrome needed, but I guess I have to back trace, or modify the code to find the
 individual palindromes
 """
+
+def string_to_fewest_palindromes(s: str) -> list:
+    assert s
+    n = len(s)
+    # p = [[False for _ in range(n)] for _ in range(n)]
+    # for i in range(n):
+    #     p[i][i] = True
+
+    # # first dp memo table for whether a substring is a palindrome
+    # for window in range(1, n):
+    #     # window will point at an inclusive end of the substring
+    #     for i in range(n):
+    #         if i + window < n:
+    #             sub_str = s[i: i + window + 1]
+    #             if sub_str[-1] == sub_str[0]:
+    #                 p[i][i + window] = True if window == 1 or p[i+1][i+window-1] else False
+
+    # second dp memo for how many palindrome is needed to cover this substring
+    c = [[1 for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        c[i][i] = 1
+
+    # # c[0][n-1] would give us the min number of palindrome to cover the whole string
+    # for window in range(1, n):
+    #     for i in range(n):
+    #         if i + window < n:
+    #             if p[i][i+window]:
+    #                 c[i][i+window] = 1
+    #             else:
+    #                 c[i][i+window] = min([c[i][i+m] + c[i+m+1][i+window] for m in range(window)])
+
+    # break ups that form the substring from i to j
+    b = [[[] for _ in range(n)] for _ in range(n)]
+    for i in range(n):
+        b[i][i].append(s[i])
+
+    for window in range(1, n):
+        for i in range(n):
+            if i + window < n:
+                sub_str = s[i: i + window + 1]
+                if sub_str[-1] == sub_str[0] and (window == 1 or c[i+1][i+window-1] == 1):
+                    c[i][i+window] = 1
+                    # b[i][i+window] = [s[i:i+window+1]]
+                else:
+                    c[i][i+window] = min([c[i][i+m] + c[i+m+1][i+window] for m in range(window)])
+
+    return []
+
+
+print(string_to_fewest_palindromes('abac'))

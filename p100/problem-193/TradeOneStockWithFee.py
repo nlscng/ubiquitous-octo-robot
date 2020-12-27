67fd896d2082c82822801487dfd304f0773b830e
nelson cheng <nlscng.g@gmail.com>
2020-12-27 06:33:07 -0600

    Problem 193, max profit trade with fees, TBC.

--- p100/problem-193/TradeOneStockWithFee.py
+++ p100/problem-193/TradeOneStockWithFee.py
@@ -18,7 +18,7 @@ needed. Like other unlimited trade problem, perhaps I can solve this with walkin
 Or perhaps the inclusion of fees turns this into a dynamic programming one.
 
 And it looks like it does. Consider this increasing sequence of prices: 1, 4, 7, 10. Greedy algorithms will buy twice,
-making profit with two fees if fee is less than 3. 
+making profit with two fees if fee is less than 3.
 
 Question 47 used as reference.
 
@@ -29,7 +29,8 @@ Question 47 used as reference.
 
 from typing import List
 
-def trade_one_stock_with_fees(prices: List[int], fee: int) -> int:
+
+def trade_with_fees_greedy(prices: List[int], fee: int) -> int:
     assert prices and len(prices) > 1
 
     max_profit = 0
@@ -47,5 +48,29 @@ def trade_one_stock_with_fees(prices: List[int], fee: int) -> int:
     return max_profit
 
 
-assert trade_one_stock_with_fees([1,3,2,8,4,10], 2) == 9
-assert trade_one_stock_with_fees([1,3,2,1,4,10], 2) == 7
+assert trade_with_fees_greedy([1, 3, 2, 8, 4, 10], 2) == 9
+
+# this next case fails on greedy version
+assert trade_with_fees_greedy([1, 3, 2, 1, 4, 10], 2) == 7
+
+"""
+So given a i-th day, and j-th price, max profit would be 
+p(i, j) = max {
+    p(i - 1, j - 1), no purchase completed on that day, aka ignore price change, 
+    max(p(m, m) + (price(i) - price(m) - 2)), 0 < m < i, assume we now hold stock of 
+        price purchased on m day, we sell and make profit today 
+} 
+"""
+
+
+# SEE: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/discuss/?currentPage=1&orderBy=most_votes&query=
+
+def trade_with_fees(prices: List[int], fee: int) -> int:
+    assert prices and len(prices) > 1
+
+    n = len(prices)
+    # A 2D memo table, where x-axis is the given price history per day, and y-axis is day 1, day 2, etc
+    # So a p(i, j), max profit of i-th day
+    memo = [[0 for _ in range(n)] for _ in range(n)]
+
+    return 0

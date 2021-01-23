022582660454a577a84e2a673e308d213b0ee8da
nelson cheng <nlscng.g@gmail.com>
2021-01-23 14:30:35 -0600

    Problme 197, added comment for some of the better solutions.

--- p100/problem-197/RotateArray.py
+++ p100/problem-197/RotateArray.py
@@ -38,22 +38,18 @@ assert array_rotate_right_linear_space([1, 2, 3, 4, 5], 2) == [3, 4, 5, 1, 2]
 assert array_rotate_right_linear_space([1, 2, 3, 4, 5], 3) == [4, 5, 1, 2, 3]
 assert array_rotate_right_linear_space([1, 2, 3, 4, 5], 4) == [5, 1, 2, 3, 4]
 
-##TBC
+#GG: So there are many other ways to do this with linear space. I could
+#   use a dictionary and keeps a mapping of value and their respective new index,
+#   or like I was using a queue.
+#   One way to do this with constant space, but O(n*k) time, is to rotate
+#   the whole array one element k times.
+#   To do this with constant space, a version of it is to reverse it a few times.
+#   Like abcde, we want to rotate right by 3, to cdeab
+#   We start reverse everything, edcba
+#   and we reverse in place, the old k+ units, the now first k+ unit, to cdeba
+#   then we reverse the old first k units, the now k+ units, to cdeab
 
-# def array_rotate_right(lis: List[int], k: int) -> List[int]:
-#     # GG: this is time of O(n) and space O(1)
-#     assert lis and 0 < k < len(lis)
-#
-#     t = None  # temp value holder, initialized to the old head value
-#     f, s = k, 0  # fast walker, slow walker
-#
-#     while f < len(lis):
-#         lis[s], t, lis[f] = lis[f], lis[s], t
-#         f += 1
-#         s += 1
-#     return lis
-#
-#
+
 # assert array_rotate_right([1, 2, 3, 4, 5], 1) == [2, 3, 4, 5, 1]
 # assert array_rotate_right([1, 2, 3, 4, 5], 2) == [3, 4, 5, 1, 2]
 # assert array_rotate_right([1, 2, 3, 4, 5], 3) == [4, 5, 1, 2, 3], "Actual: {}".format(

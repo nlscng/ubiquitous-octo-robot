0d4d429468dd1af77cb94ca8889ca9a8fa477704
nelson cheng <nlscng.g@gmail.com>
2021-01-25 14:16:23 -0600

    Problem 198, larget subset of mutually divisible elements in an array.

--- p100/problem-198/LargestDivisibleSubset.py
+++ p100/problem-198/LargestDivisibleSubset.py
@@ -24,3 +24,35 @@ this as a LIS DP problem, where condition add one to the number of the subset is
 where or not two values are mutually divisible.
 """
 
+from typing import List
+
+
+def mutually_divisible(a: int, b: int) -> bool:
+    return a % b == 0 if a >= b else b % a == 0
+
+
+def largest_divisible_subset(lis: List[int]) -> List[int]:
+    # This involves a sort at first to help the divisibility test, so that's
+    # O(n log n) in time. And I have a for loop with a max function to scan, so
+    # that's O(n^2) in time. The memo table is 1D, so O(n) in space. And at the end
+    # I did another scan to pick up the elements in this largest group, so another
+    # O(n) in time.
+    # Overall this is O(n^2) in time and O(n) in space
+    assert lis
+    lis.sort()  # this makes this function non pure
+    n = len(lis)
+    memo = [0] * n   # each cell at i-th is to keep counts of number of divisible members that includes value at i-th
+    memo[0] = 1
+    for i in range(1, n):
+        div_able_count = [memo[x] for x in range(0, i) if mutually_divisible(lis[x], lis[i])]
+        if len(div_able_count) > 0:
+            memo[i] = max(div_able_count) + 1
+        else:
+            memo[i] = 1
+
+    largest_idx = memo.index(max(memo))
+    return [x for x in lis if mutually_divisible(x, lis[largest_idx])]
+
+
+assert largest_divisible_subset([3, 5, 10, 20, 21]) == [5, 10, 20]
+assert largest_divisible_subset([1, 3, 6, 24]) == [1, 3, 6, 24]

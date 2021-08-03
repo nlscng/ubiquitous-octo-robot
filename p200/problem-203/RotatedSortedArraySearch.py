68ac62b4c50581d7bb501e2393a4b9ed4418fa5a
Nelson Cheng <nelsoncheng@fb.com>
2021-08-03 23:32:56 +0800

    P203, find min element in a list of sorted, unique int in log(n) time.

--- p200/problem-203/RotatedSortedArraySearch.py
+++ p200/problem-203/RotatedSortedArraySearch.py
@@ -6,27 +6,28 @@
 from typing import Optional
 
 
-def rotated_sorted_array_search(array: list) -> Optional[int]:
-    assert len(array) > 0
+def rotated_sorted_array_search(arr: list) -> Optional[int]:
+    assert len(arr) > 0
 
-    b, e = 0, len(array) - 1  ## e needs to be inclusive
+    b, e = 0, len(arr) - 1  ## e needs to be inclusive
 
     while b < e:
-        if array[b] > array[e] and e - b == 1:
-            return e
+        if abs(b - e) == 1:
+            return min(arr[b], arr[e])
 
         m = (b + e) // 2
-        if array[b] < array[m] < array[e]:
-            return array[b]
-        elif array[b] > array[m]:
+        if arr[b] < arr[e]:
+            return arr[b]
+        elif arr[b] > arr[m]:
             e = m
         else:
             b = m + 1
 
-    return array[b]
+    return arr[b]
 
 
 assert rotated_sorted_array_search([1, 2, 3]) == 1
 assert rotated_sorted_array_search([1]) == 1
 assert rotated_sorted_array_search([3, 1, 2]) == 1
 assert rotated_sorted_array_search([3, 4, 1, 2]) == 1
+assert rotated_sorted_array_search([4,5,6,2,3]) == 2
